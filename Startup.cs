5a072020a557177b746034a49d8075a0ee9b3e9a
Igor Music <igormusich@gmail.com>
2020-05-23 05:49:40 -0400

    upgrade to release version of Blazor

--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,3 @@
-using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace MatDesignStart
@@ -8,10 +7,6 @@ namespace MatDesignStart
         public void ConfigureServices(IServiceCollection services)
         {
         }
-
-        public void Configure(IComponentsApplicationBuilder app)
-        {
-            app.AddComponent<App>("app");
-        }
+        
     }
 }
